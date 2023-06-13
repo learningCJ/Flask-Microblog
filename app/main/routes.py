1a29ede8a13e8a3073e9f4583ef7c24eaacef3f9
cj <99549507+learningCJ@users.noreply.github.com>
2023-06-13 15:25:42 -0400

    bug fixing unconfirmed user

--- app/main/routes.py
+++ app/main/routes.py
@@ -28,7 +28,6 @@ def index():
     if form.validate_on_submit():
         if not current_user.isVerified:
             flash(_('You can only post when you confirm your account. Please check your email or resend confirmation email from your profile page'))
-            persistentMsg = form.post.data
             return redirect(url_for('main.index'))
         try:
             language = detect(form.post.data)
