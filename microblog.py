8cb4651afc7a293db24f1cc7357eb6510bd5e2df
cj <99549507+learningCJ@users.noreply.github.com>
2023-07-25 17:25:48 -0400

    updated flask shell context and increased the char count for article

--- microblog.py
+++ microblog.py
@@ -1,10 +1,11 @@
 from app import create_app,db,cli
-from app.models import User, Post
+from app.models import User, Post, Article, Comment, Tag
+import sqlalchemy as sa
 
 app = create_app()
 cli.register(app)
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User, 'Post': Post}
+    return {'db': db, 'User': User, 'Post': Post, 'Article':Article, 'Comment':Comment,'Tag':Tag, 'sa':sa}
 
