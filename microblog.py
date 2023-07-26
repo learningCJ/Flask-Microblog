2d5e1138a389fa3c05811219c1d1aeffd4de638d
cj <99549507+learningCJ@users.noreply.github.com>
2023-07-26 17:19:18 -0400

    updated shell context and updated css

--- microblog.py
+++ microblog.py
@@ -1,5 +1,5 @@
 from app import create_app,db,cli
-from app.models import User, Post, Article, Comment, Tag
+from app.models import User, Post, Article, Comment, Tag, post_tags
 import sqlalchemy as sa
 
 app = create_app()
@@ -7,5 +7,5 @@ cli.register(app)
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User, 'Post': Post, 'Article':Article, 'Comment':Comment,'Tag':Tag, 'sa':sa}
+    return {'db': db, 'User': User, 'Post': Post, 'Article':Article, 'Comment':Comment,'Tag':Tag, 'sa':sa, 'post_tags':post_tags}
 
